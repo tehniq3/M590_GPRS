875b06d2c83c2b1a6a4c7a8b1ff7dd7b2547cc0b
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2015-08-17 09:33:43 +0300

    Update and rename .gitignore to M590_Mega_serialmonitor.ino

---
+++ M590_Mega_serialmonitor.ino
@@ -0,0 +1,18 @@
+// original sketch: http://shin-ajaran.blogspot.ro/2014/12/noobs-guide-to-esp8266-with-arduino.html
+// rewrite after photo for ESP8266 Wi-Fi module by niq_ro from http://nicuflorica.blogspot.com/
+// and http://arduinotehniq.blogspot.com/
+// and http://www.tehnic.go.ro
+// rewrite for M590 GPRS module by niq_ro - 13.08.2015, Craiova - Romania, Europe, Earth
+
+void setup() {
+  // initialize both serial ports:
+  Serial.begin(115200);
+  Serial2.begin(115200);
+//Serial2.begin(9600);
+}
+
+void loop() {
+ while(Serial2.available()) Serial.write(Serial2.read());
+ 
+while(Serial.available()) Serial2.write(Serial.read());
+}
